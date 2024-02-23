89fbdc73ce698e3ecd8b79cdb7018bd742ce003d
Talisma Manuel <talismacassoma.manuel@uit.ac.ma>
2024-02-23 13:11:27 +0100

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://talisma-cassoma.github.io/our-money/"
 })
