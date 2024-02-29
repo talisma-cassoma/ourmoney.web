931eaecea1fdb6999373e16a2e6f004dbd102282
Talisma Manuel <talismacassoma.manuel@uit.ac.ma>
2024-03-01 00:26:16 +0100

    create login pape

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import { createRoot } from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import { App } from "./App";
+import { Login } from "./pages/login/components";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById("root");
+createRoot(rootElement).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="our-money/" element={<Login />} />
+      <Route path="/our-money/transactions" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
---
+++ src/pages/login/components/index.tsx
@@ -0,0 +1,35 @@
+
+import logoImg from '../../../assets/logo.svg';
+import googleIconImg from '../../../assets/google-icon.svg';
+
+import { Button } from '../../../components/Button';
+
+import './styles.scss';
+
+export function Login() {
+  return (
+    <div id="page-auth">
+      <main className="main-content">
+        <img src={logoImg} alt="our-money" />
+        <button className="login">
+          <img src={googleIconImg} alt="Logo do Google" />
+          Login com o Google
+        </button>
+        <div className="separator">ou digite sua palavra passe</div>
+        <form>
+          <input
+            type="text"
+            placeholder="nome usaurio ou email"
+          />
+          <input
+            type="text"
+            placeholder="Digite sua palavra-passe"
+          />
+          <Button type="submit">
+            ENTRAR
+          </Button>
+        </form>
+      </main>
+    </div>
+  )
+}
