3972f354630e284574947c06b9e74b3e12b29297
Talisma Manuel <talismacassoma.manuel@uit.ac.ma>
2024-03-03 22:16:01 +0100

    fixiing deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,17 @@
-import { createRoot } from "react-dom/client";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import { App } from "./App";
-import { Login } from "./pages/login/components";
-import { AuthContextProvider } from "./contexts/AuthContext";
+import { createRoot } from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { App } from './App';
+import { Login } from './pages/login/components';
+import { AuthContextProvider } from './contexts/AuthContext';
 
-
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 createRoot(rootElement).render(
   <BrowserRouter>
-   <AuthContextProvider>
-    <Routes>
-      <Route path="/our-money/" element={<Login />} />
-      <Route path="/our-money/transactions" element={<App />} />
-    </Routes>
+    <AuthContextProvider>
+      <Routes>
+        <Route path="/our-money/" element={<Login />} />
+        <Route path="/our-money/transactions" element={<App />} />
+      </Routes>
     </AuthContextProvider>
   </BrowserRouter>
 );
