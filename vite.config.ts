beaad7fdbc964bc69cc5fccc5449c5d50e88e1c0
talisma-cassoma <talismacassoma.manuel@uit.ac.ma>
2025-04-03 17:42:12 +0000

    fix: dist deps

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ourmoney/"
+  base: "/ourmoney/"  // Certifique-se de que "/ourmoney/" é o caminho correto para o seu servidor
 })
-
